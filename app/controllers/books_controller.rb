71b4a77017ff19e877a43f58f4ddaefcdf02e6fa
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-10-19 14:26:40 +0000

    [add]createの改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
   private
 
    def book_params
-     params.require(:book).permit(:book_name, :caption)
+     params.require(:book).permit(:title, :body)
    end
 
 
