df221d9b7c32209e13c359416701c0209e89ab13
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-10-19 00:47:11 +0000

    [add]Bootstrapインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_18_171045) do
+ActiveRecord::Schema.define(version: 2025_10_19_004339) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,9 +42,10 @@ ActiveRecord::Schema.define(version: 2025_10_18_171045) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 2025_10_18_171045) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
