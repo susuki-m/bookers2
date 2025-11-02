e0fa6b17bd31e8dcee9d52e154b15bb418cdfe61
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-11-02 12:20:31 +0000

    [add]ログイン時の入力を名前に変更しログインできるようになった

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:name, :email]
+         :recoverable, :rememberable, :validatable, authentication_keys: [:name]
 
   has_many :books, dependent: :destroy
 
