2e8021e5a18b39ef70463e04f637be728eb82c70
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-11-03 06:13:14 +0000

    [add]books/show作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
     @users = User.all
+
     
   end
 
