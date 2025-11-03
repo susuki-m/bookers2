98bed9fdfee22f60e402e9669de9bb8d381dba4c
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-11-03 09:24:50 +0000

    [add]users/index途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,20 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:title, :body, :image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to root_path
+    end
+  end
+
 
 end
